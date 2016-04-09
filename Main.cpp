6b62dfda8f1464ff06498655162a1d536eca1991
Martin Tang <neuks@qq.com>
2016-04-09 18:55:06 +0800

    added K-Line Merging algorithm (both forward and backward)
    Signed-off-by: Martin Tang <neuks@qq.com>

--- Main.cpp
+++ Main.cpp
@@ -81,6 +81,7 @@ void Parse1(int nCount, float *pOut, float *pHigh, float *pLow)
 // 化简函数（至少5根K线完成一笔）
 int Parse2(int nCount, float *pOut, float *pHigh, float *pLow)
 {
+  int nSpan = 0;
   int nCurrTop = 0, nPrevTop = 0;
   int nCurrBot = 0, nPrevBot = 0;
 
@@ -93,20 +94,67 @@ int Parse2(int nCount, float *pOut, float *pHigh, float *pLow)
       nPrevTop = nCurrTop;
       nCurrTop = i;
 
-      // 检查合法性（严格按照连续五根形成一笔）
-      if (((nCurrTop - nCurrBot < 4) && (nCount   - nCurrTop > 4)) ||
-           (nCurrBot - nPrevTop < 4) || (nPrevTop - nPrevBot < 4))
+      // 存在小于五根的线段，去除中间一段
+      if ((pHigh[nCurrTop] >= pHigh[nPrevTop]) &&
+          (pLow [nCurrBot] >  pLow [nPrevBot]))
       {
-        // 存在小于五根的线段，去除中间一段
-        if ((pHigh[nCurrTop] >= pHigh[nPrevTop]) &&
-            (pLow [nCurrBot] >  pLow [nPrevBot]))
+        // 检查合法性（严格按照连续五根形成一笔）
+        if (((nCurrTop - nCurrBot < 4) && (nCount   - nCurrTop > 4)) ||
+             (nCurrBot - nPrevTop < 4) || (nPrevTop - nPrevBot < 4))
         {
           pOut[nCurrBot] = 0;
           pOut[nPrevTop] = 0;
         }
-      }
-      else
-      {
+        else
+        {
+          // 检查第三段（上）K线合并
+          nSpan = nCurrTop - nCurrBot;
+          for (int j = nCurrBot; j < nCurrTop; j++)
+          {
+            if (((pHigh[j] <= pHigh[j+1]) && (pLow[j] >= pLow[j+1])) ||
+                ((pHigh[j] >= pHigh[j+1]) && (pLow[j] <= pLow[j+1])))
+            {
+              nSpan--;
+            }
+          }
+          if (nSpan < 4)
+          {
+            pOut[nCurrBot] = 0;
+            pOut[nPrevTop] = 0;
+          }
+
+          // 检查第二段（下）K线合并
+          nSpan = nCurrBot - nPrevTop;
+          for (int j = nPrevTop; j < nCurrBot; j++)
+          {
+            if (((pHigh[j] <= pHigh[j+1]) && (pLow[j] >= pLow[j+1])) ||
+                ((pHigh[j] >= pHigh[j+1]) && (pLow[j] <= pLow[j+1])))
+            {
+              nSpan--;
+            }
+          }
+          if (nSpan < 4)
+          {
+            pOut[nCurrBot] = 0;
+            pOut[nPrevTop] = 0;
+          }
+
+          // 检查第一段（上）K线合并
+          nSpan = nPrevTop - nPrevBot;
+          for (int j = nPrevBot; j < nPrevTop; j++)
+          {
+            if (((pHigh[j] <= pHigh[j+1]) && (pLow[j] >= pLow[j+1])) ||
+                ((pHigh[j] >= pHigh[j+1]) && (pLow[j] <= pLow[j+1])))
+            {
+              nSpan--;
+            }
+          }
+          if (nSpan < 4)
+          {
+            pOut[nCurrBot] = 0;
+            pOut[nPrevTop] = 0;
+          }
+        }
       }
     }
 
@@ -117,20 +165,67 @@ int Parse2(int nCount, float *pOut, float *pHigh, float *pLow)
       nPrevBot = nCurrBot;
       nCurrBot = i;
 
-      // 检查合法性（严格按照连续五根形成一笔）
-      if (((nCurrBot - nCurrTop < 4) && (nCount   - nCurrBot > 4)) ||
-           (nCurrTop - nPrevBot < 4) || (nPrevBot - nPrevTop < 4))
+      // 存在小于五根的线段，去除中间一段
+      if ((pLow [nCurrBot] <= pLow [nPrevBot]) &&
+          (pHigh[nCurrTop] <  pHigh[nPrevTop]))
       {
-        // 存在小于五根的线段，去除中间一段
-        if ((pLow [nCurrBot] <= pLow [nPrevBot]) &&
-            (pHigh[nCurrTop] <  pHigh[nPrevTop]))
+        // 检查合法性（严格按照连续五根形成一笔）
+        if (((nCurrBot - nCurrTop < 4) && (nCount   - nCurrBot > 4)) ||
+             (nCurrTop - nPrevBot < 4) || (nPrevBot - nPrevTop < 4))
         {
           pOut[nCurrTop] = 0;
           pOut[nPrevBot] = 0;
         }
-      }
-      else
-      {
+        else
+        {
+          // 检查第三段（下）K线合并
+          nSpan = nCurrBot - nCurrTop;
+          for (int j = nCurrTop; j < nCurrBot; j++)
+          {
+            if (((pHigh[j] <= pHigh[j+1]) && (pLow[j] >= pLow[j+1])) ||
+                ((pHigh[j] >= pHigh[j+1]) && (pLow[j] <= pLow[j+1])))
+            {
+              nSpan--;
+            }
+          }
+          if (nSpan < 4)
+          {
+            pOut[nCurrTop] = 0;
+            pOut[nPrevBot] = 0;
+          }
+
+          // 检查第二段（上）K线合并
+          nSpan = nCurrTop - nPrevBot;
+          for (int j = nPrevBot; j < nCurrTop; j++)
+          {
+            if (((pHigh[j] <= pHigh[j+1]) && (pLow[j] >= pLow[j+1])) ||
+                ((pHigh[j] >= pHigh[j+1]) && (pLow[j] <= pLow[j+1])))
+            {
+              nSpan--;
+            }
+          }
+          if (nSpan < 4)
+          {
+            pOut[nCurrTop] = 0;
+            pOut[nPrevBot] = 0;
+          }
+
+          // 检查第一段（下）K线合并
+          nSpan = nPrevBot - nPrevTop;
+          for (int j = nPrevTop; j < nPrevBot; j++)
+          {
+            if (((pHigh[j] <= pHigh[j+1]) && (pLow[j] >= pLow[j+1])) ||
+                ((pHigh[j] >= pHigh[j+1]) && (pLow[j] <= pLow[j+1])))
+            {
+              nSpan--;
+            }
+          }
+          if (nSpan < 4)
+          {
+            pOut[nCurrTop] = 0;
+            pOut[nPrevBot] = 0;
+          }
+        }
       }
     }
   }
